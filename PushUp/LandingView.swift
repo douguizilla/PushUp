fabc59f0e34f15f8d9fdbb025f1166449556f250
Douglas Gomes <douglas.gomes@optimumsolucoes.com>
2021-12-20 17:16:18 -0500

    Align  app title

--- PushUp/LandingView.swift
+++ PushUp/LandingView.swift
@@ -9,14 +9,38 @@ import SwiftUI
 
 struct LandingView: View {
     var body: some View {
-        VStack{
-            Text("Hello")
+        GeometryReader{ proxy in
+            VStack{
+                Spacer()
+                    .frame(
+                        height: proxy.size.height * 0.2
+                    )
+                Text("PushUp")
+                    .font(
+                        .system(
+                            size: 64,
+                            weight: .medium
+                        )
+                    )
+                Spacer()
+            }
+            .frame(
+                maxWidth: .infinity,
+                maxHeight: .infinity
+            )
+            .background(
+                Image("pushup")
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    
+            )
+            .edgesIgnoringSafeArea(.all)
         }
     }
 }
 
 struct LandingView_Previews: PreviewProvider {
     static var previews: some View {
-        LandingView()
+        LandingView().previewDevice("iPhone 11 Pro")
     }
 }
