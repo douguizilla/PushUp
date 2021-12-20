5d9bcdc51c6b286aaf2cb4e58a5e64d4f84fe4d0
Douglas Gomes <douglas.gomes@optimumsolucoes.com>
2021-12-20 16:48:43 -0500

    add project files

---
+++ PushUp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PushUp
+//
+//  Created by user198901 on 20/12/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PushUp/PushUpApp.swift
@@ -0,0 +1,17 @@
+//
+//  PushUpApp.swift
+//  PushUp
+//
+//  Created by user198901 on 20/12/21.
+//
+
+import SwiftUI
+
+@main
+struct PushUpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
